517d0a5e089626231d581978bacddaab7e361325
David <klingesd@si.edu>
2024-06-04 11:41:34 -0400

    update power analysis

--- scripts/02_power_analysis/power_analysis.R
+++ scripts/02_power_analysis/power_analysis.R
@@ -23,13 +23,14 @@ cat("Conducting power analysis...\n")
 
 # k=3 predictors, n=100 observations, R^2 = .1, sig.level = .05 (default)
 
+
 if (all(complete.cases(chosen_layers))) {
   n_predictors <- length(chosen_layers)
 }
 
-if (complete.cases(n_sensors, n_predictors)) {
+if (complete.cases(n_sites, n_predictors)) {
   pwr_out <- pwr.f2.test(u = n_predictors - 1,
-                         v = n_sensors - n_predictors,
+                         v = n_sites - n_predictors,
                          f2 = r2 / (1 - r2),
                          sig.level = 0.05,
                          power = power) 
@@ -37,7 +38,7 @@ if (complete.cases(n_sensors, n_predictors)) {
 
 if (all(is.na(chosen_layers))) {
   pwr_out <- pwr.f2.test(u = NULL,
-                         v = n_sensors,
+                         v = n_sites,
                          f2 = r2 / (1 - r2),
                          sig.level = 0.05,
                          power = power)
@@ -45,7 +46,7 @@ if (all(is.na(chosen_layers))) {
   cat("According to power analysis with your specified inputs, you can afford to have", floor(pwr_out$u + 1), "predictors provided to chosen_layers (set_parameters.R; L82).\n")
 }
 
-if (is.na(n_sensors)) {
+if (is.na(n_sites)) {
   pwr_out <- pwr.f2.test(u = n_predictors - 1,
                          v = NULL,
                          f2 = r2 / (1 - r2),
@@ -53,16 +54,16 @@ if (is.na(n_sensors)) {
                          power = power)
   
   if (all(complete.cases(c(budget, cost_per_sensor)))) {
-    n_sensors_max <- budget / cost_per_sensor
+    n_sites_max <- budget / cost_per_sensor
     
-    n_sensors <- ceiling(pwr_out$v - n_predictors)
-    if (n_sensors > n_sensors_max) {
-      warning("Your provided budget only allows for ", n_sensors_max, " sensors, but the combination of the number of chosen_layers, r2, and power all would require at least ", n_sensors, " sensors. We strongly suggest modifying your inputs.")
+    n_sites <- ceiling(pwr_out$v - n_predictors)
+    if (n_sites > n_sites_max) {
+      warning("Your provided budget only allows for ", n_sites_max, " sensors, but the combination of the number of chosen_layers, r2, and power all would require at least ", n_sites, " sensors. We strongly suggest modifying your inputs.")
     }
   }
 }
 
-if (is.na(power) | is.null(power)) {
+if (is.null(power)) {
   if (pwr_out$power < 0.5) {
     stop("Your chosen set of parameters has a power estimate of ", round(pwr_out$power, 2),
          ", which is too low (less than 0.5)")
@@ -77,5 +78,4 @@ if (is.na(power) | is.null(power)) {
     cat("Your chosen set of parameters has a power estimate of ", round(pwr_out$power, 2),
         ", which is high, nice!")
   } 
-  
 }
