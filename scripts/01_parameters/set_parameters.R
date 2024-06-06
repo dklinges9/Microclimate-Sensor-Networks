1e154447c1c4cd287ae132d5db46ff0e3dc14c30
David <klingesd@si.edu>
2024-06-06 13:05:50 -0400

    trying new regions, re-ordering parameters

--- scripts/01_parameters/set_parameters.R
+++ scripts/01_parameters/set_parameters.R
@@ -13,11 +13,11 @@ cat("Setting parameters...\n")
 
 # What is the name of the landscape? Will be appended to names of output files
 # If left NA will names files according to the chosen spatial extent
-landscape_name <- "gnv"
+landscape_name <- "flanders"
 
 # Your budget for sensors. This will be used to constrain the amount of 
 # environmental space that you can sample
-budget <- 5000
+budget <- 15000
 cost_per_sensor <- 100
 
 # Approximate number of sites to select, at which a sensor will be placed
@@ -29,7 +29,7 @@ cost_per_sensor <- 100
 
 # Also note that depending on your study design, you may want to place multiple
 # sensors at a given spatial point (e.g. at different heights/depths).
-n_sites <- 100
+n_sites <- 150
 
 ## Spatial parameters ------------
 
@@ -37,9 +37,14 @@ n_sites <- 100
 # Suggest specifying an extent that is slightly larger than actual target extent,
 # to account for lost edges when reprojecting and when calculating topographical
 # variables 
-spatial_extent <- c(-82.407035, -82.306098, 29.641339, 29.704579)
-  # Ambalavero: c(47.4, 47.55, -21.6, -21.45)
+spatial_extent <- c(170939.777724312, 518527.501269666, 5355763.70341275, 5493712.58119482)
+  # Oman: c(57.0355415770998, 58.0812496067212, 22.9244067159718, 23.4996527359249)
+  # Flanders, with buffer: c(170939.777724312, 518527.501269666, 5355763.70341275, 5493712.58119482)
+  # c(-82.407035, -82.306098, 29.641339, 29.704579) Gainesville
+  # Madagascar: c(47.4, 47.55, -21.6, -21.45)
 # c(5042788.27696657, 5064493.44436001, 1817708.23946404, 1850265.99055419) # c(5042788.27696657, 5064493.44436001, 1817708.23946404, 1850265.99055419) # c(-83.8, -83.3, 35.3, 35.8) # c(748472.066251046, 764572.066251046, -2389957.46406275, -2373857.46406275) #  # provide also in meters 
+
+
 # Alternatively a path to a raster file or shapefile can be provided
 # (I HAVEN'T YET MADE THESE OPTIONS FUNCTIONAL)
 extent_rast <- NA
@@ -51,31 +56,18 @@ extent_shp <- NA
 # "+proj=longlat +datum=WGS84" 
 # Note that projections with unites in meters will entail much faster access
 # of a Digital Elevation Model (DEM)
-projection <-  "+proj=longlat +datum=WGS84" 
-projection_units <- "dd" # c("m", "dd") for 'meters' and 'decimal degrees'
+projection <-  "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs" 
+projection_units <- "m" # c("m", "dd") for 'meters' and 'decimal degrees'
 
 # Your desired resolution of the Digital Elevation Model (DEM). Must be at 
 # least 30 meters. This will also determine the resolution at which all 
 # analyses are conducted
 # The following few parameters can be divided by 120000 for a quick
 # translation from meters to decimal degrees
-chosen_rez <- 100 / 120000 # in same units as projection in meters
-min_distance <- 100 / 120000 # min tolerable distance between points, in projection_units
-max_distance <- 3500 / 120000 # max tolerable distance between one point and the
+chosen_rez <- 100 # / 120000 # in same units as projection in meters
+min_distance <- 100 # / 120000 # min tolerable distance between points, in projection_units
+max_distance <- 10000 # / 120000 # max tolerable distance between one point and the
 # next nearest point, in projection_units
-
-## Power analysis parameters ----------
-
-# Estimate of power (1 minus Type II error probability). If unknown, keep at 0.8
-# OR is calculated
-power <- NA # 0.8 # NULL
-
-# Estimate of the explanatory power (r2) of chosen set of predictors for
-# explaining response variable of interest (that is measured by sensors), OR
-# desired r2. 
-# Used for calculating f2 (effect size) in power analysis. If unknown, kept at 0.15
-# (reasonably cautious for most environmental relationships)
-r2 <- 0.15
 
 ## Environmental layers ---------
 # chosen_layers must be a string of the names of a subset (or all) of the following:
@@ -112,13 +104,26 @@ custom_layers_names <- NA
 # Must have numeric values
 # Can be any resolution
 
-layer_mask <- rast("data/spatial_drivers/topography/derivative/aspect_4e-04_amba2.tif")
-terra::values(layer_mask) <- round(runif(length(terra::values(layer_mask)), min= 0, max = 1))
+# layer_mask <- rast("data/spatial_drivers/topography/derivative/aspect_4e-04_amba2.tif")
+# terra::values(layer_mask) <- round(runif(length(terra::values(layer_mask)), min= 0, max = 1))
 layer_mask <- NA
 
 # When selecting sites, do you want to give more weight to outliers within 
 # the environmental space (e.g. more topographically complex areas)?
 favor_outliers <- FALSE
+
+## Power analysis parameters ----------
+
+# Estimate of power (1 minus Type II error probability). If unknown, keep at 0.8
+# OR is calculated
+power <- NA # 0.8 # NULL
+
+# Estimate of the explanatory power (r2) of chosen set of predictors for
+# explaining response variable of interest (that is measured by sensors), OR
+# desired r2. 
+# Used for calculating f2 (effect size) in power analysis. If unknown, kept at 0.15
+# (reasonably cautious for most environmental relationships)
+r2 <- 0.15
 
 ## Required sites -----------
 
@@ -129,9 +134,9 @@ favor_outliers <- FALSE
 # dim2_bin -- Dim. 2 bin that the coordinate falls into, as created on last program run
 # dim3_bin -- Dim. 3 bin that the coordinate falls into, as created on last program run
 # (other columns are also allowed but ignored)
-# required_sites <- NA
-required_sites <- read_csv("data/chosen_sites/selected_sites_30_gnv.csv") %>%
-  dplyr::sample_n(10)
+required_sites <- NA
+# required_sites <- read_csv("data/chosen_sites/selected_sites_30_gnv.csv") %>%
+#   dplyr::sample_n(10)
 # For example:
 # required_sites <- read_csv("data/chosen_points/selected_points_150_-83.8_-83.3_35.3_35.8.csv")
 
