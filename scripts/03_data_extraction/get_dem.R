b188d9e3e22883bbc36345999816c0817285f68e
David <klingesd@si.edu>
2024-07-04 10:10:47 -0400

    update check if should run get_dem

--- scripts/03_data_extraction/get_dem.R
+++ scripts/03_data_extraction/get_dem.R
@@ -12,9 +12,13 @@ for (i in seq_along(pkgs)) {
   if (i == length(pkgs)) { rm(pkgs, i) }
 }
 
-# Confirm script should be run
-if (program_rerun) {
-  cat(red("program_rerun set to TRUE, so you should not need to re-run get_dem.R. Do you wish to continue? (Y/N): "))
+# As querying DEM may be time consuming, first check to see if program_rerun, or
+# if DEM of same name already exists, and if so then prompt user
+if (program_rerun | file.exists(paste0("data/spatial_drivers/topography/derivative/dem_", 
+                                       round(chosen_rez, 4), "_", filepattern, ".tif"))) {
+  cat(red("Either `program_rerun` is set to TRUE and/or DEM file", paste0("data/spatial_drivers/topography/derivative/dem_", 
+                                                                    round(chosen_rez, 4), "_", filepattern, ".tif"), 
+          "already exists, so you should not need to re-run get_dem.R. Do you wish to continue? (Y/N): "))
   ans1 <- readline(" ")
   
   if (!tolower(ans1) %in% c("n", "no", "y", "yes")) {
@@ -33,7 +37,9 @@ if (program_rerun) {
   continue <- TRUE
 }
 
+
 if (continue) {
+  
   spatial_extentr <- terra::rast(ext(spatial_extent), resolution = chosen_rez)
   crs(spatial_extentr) <- projection
   
