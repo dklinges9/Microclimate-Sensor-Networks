2574a78313513d27f287d8ad23117b2796ce2c9e
David <klingesd@si.edu>
2024-09-21 12:02:35 -0400

    vanity fixes

--- scripts/00_source/functions.R
+++ scripts/00_source/functions.R
@@ -7,16 +7,16 @@ save_raster <- function(rst, filepath, filepattern) {
   if (file.exists(paste0(filepath,
                          filepattern, ".tif"))) {
     cat(crayon::red("File ", 
-            paste0(filepath,
-                   filepattern, ".tif"), 
-            " already exists. Overwrite? (Y/N): "))
+                    paste0(filepath,
+                           filepattern, ".tif"), 
+                    " already exists. Overwrite? (Y/N): "))
     
     ans1 <- readline(paste0(" "))
     
     if (tolower(ans1) %in% c("y", "yes")) {
       cat("File overwritten, continuing program.\n")
       writeRaster(rst, paste0(filepath,
-                                    filepattern, ".tif"), overwrite = T)
+                              filepattern, ".tif"), overwrite = T)
     }
     if (tolower(ans1) %in% c("n", "no")) {
       cat("File not overwritten, continuing program.\n")
@@ -25,7 +25,7 @@ save_raster <- function(rst, filepath, filepattern) {
     if (!file.exists(dirname(filepath))) {
       cat(red("Directory `", dirname(filepath), "` does not yet exist.\n Create directory? (Y/N)\n"))
       ans2 <- readline(" ")
-
+      
       if (tolower(ans2) %in% c("y", "yes")) {
         dir.create(dirname(filepath), recursive = TRUE)
         cat(red("Creating directory: ", dirname(filepath)), "\n")
@@ -35,7 +35,7 @@ save_raster <- function(rst, filepath, filepattern) {
       
     }
     writeRaster(rst, paste0(filepath,
-                                  filepattern, ".tif"))
+                            filepattern, ".tif"))
   }
 }
 
